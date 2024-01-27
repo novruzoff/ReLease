272c545424fe5516504826da6cd082a0a9c8ff8e
Emilia Krichevsky <emiliakrichevsky@gmail.com>
2024-01-27 18:49:29 -0500

    Add animation to 404 pages

--- app/pages/not_found_404.py
+++ app/pages/not_found_404.py
@@ -1,6 +1,27 @@
 import dash
-from dash import html
+import dash_bootstrap_components as dbc
+import dash_extensions as de
 
-dash.register_page(__name__)
+URL = "https://lottie.host/bf4a2e06-a24d-4c12-b635-6cc73074f211/YD7GpocDk0.json"
 
-layout = html.H1("This is our custom 404 content")
+options = dict(loop=True, autoplay=True)
+
+dash.register_page(__name__, path="/404", title="Not Found")
+
+
+layout = dbc.Container(
+    [
+        de.Lottie(
+            url=URL,
+            options=options,
+            isClickToPauseDisabled=True,
+            width="700px",
+        )
+    ],
+    style={
+        "display": "flex",
+        "justify-content": "center",
+        "align-items": "center",
+        "height": "100vh",
+    },
+)
