6ea68ebdf6bce27af8bae06def1da0291ecc240c
Emilia Krichevsky <emiliakrichevsky@gmail.com>
2024-01-27 14:47:17 -0500

    Create skeleton file for database operations

---
+++ app/server/functions.py
@@ -0,0 +1,11 @@
+# Common functions for the server to perform CRUD operations on the database
+
+# Get a collection from the database
+
+# Get all documents from a collection
+
+# Count the number of documents in a collection
+
+# Get a single document from a collection
+
+# Query for a single document from a collection
