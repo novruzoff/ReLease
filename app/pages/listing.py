f1c50b4ab606d0ab3c6dfe24a56915ae4fbe4fc2
Emilia Krichevsky <emiliakrichevsky@gmail.com>
2024-01-28 01:44:27 -0500

    todo

--- app/pages/listing.py
+++ app/pages/listing.py
@@ -2,13 +2,14 @@
 import dash
 from dash import Input, Output, html, callback
 import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
 from layout import layout as com
 
 dash.register_page(__name__, path='/listing', title='Listing', name='Listing')
 
 layout = dbc.Container([
     html.H1('Listing!'),
-    # html.Div('Browse our catalog of listings, from Montrealers to Montrealers'),
-    # com.group
+    # find_one_document
+    # display all info
     
 ])
