89b1d429e4ea85de1a7d377b9443894ee3fa6d30
Emilia Krichevsky <emiliakrichevsky@gmail.com>
2024-01-27 19:15:27 -0500

    Individual listing page

--- app/pages/listing.py
+++ app/pages/listing.py
@@ -1,1 +1,14 @@
 # Page to display a single listing
+import dash
+from dash import Input, Output, html, callback
+import dash_bootstrap_components as dbc
+from layout import layout as com
+
+dash.register_page(__name__, path='/listing', title='Listing', name='Listing')
+
+layout = dbc.Container([
+    html.H1('Listing!'),
+    # html.Div('Browse our catalog of listings, from Montrealers to Montrealers'),
+    # com.group
+    
+])
