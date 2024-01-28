6329adebb1b47e54eb22ccf29ba982e651aafc75
Emilia Krichevsky <emiliakrichevsky@gmail.com>
2024-01-28 00:59:26 -0500

    Fix import

--- app/layout/layout.py
+++ app/layout/layout.py
@@ -1,7 +1,6 @@
 from dash import html
 import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
-import dash_mantine_components as dmc
 
 grid = dmc.SimpleGrid(id="grid", cols=3, spacing='lg', children=[])
 
