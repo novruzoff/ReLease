4865c247504bca758013d35865b8af52dfafa3f4
Emilia Krichevsky <emiliakrichevsky@gmail.com>
2024-01-27 19:15:04 -0500

    Update about pages

--- app/pages/about.py
+++ app/pages/about.py
@@ -1,1 +1,12 @@
 """About page. This page is a placeholder for the about page."""
+import dash
+from dash import Input, Output, html, callback
+import dash_bootstrap_components as dbc
+from layout import layout as com
+
+dash.register_page(__name__, path='/about', title='About', name='About')
+
+layout = dbc.Container([
+    html.H1('About ReLease!'),
+    
+])
